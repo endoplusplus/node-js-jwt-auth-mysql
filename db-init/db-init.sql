07bb9804409277755d7a13664922c5e8c2c18222
eddy-dot <eddy.valverde.garro@gmail.com>
2020-09-30 22:45:26 -0600

    fix(db): add missing db tables

--- db-init/db-init.sql
+++ db-init/db-init.sql
@@ -1,1 +1,37 @@
 CREATE DATABASE testdb;
+USE testdb;
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP
+);
+
+TRUNCATE roles;
+INSERT INTO roles (id, name) VALUES
+(1,	'ROLE_USER'),
+(2,	'ROLE_MODERATOR'),
+(3,	'ROLE_ADMIN');
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id bigint AUTO_INCREMENT PRIMARY KEY,
+    email character varying(50),
+    password character varying(120),
+    username character varying(20),
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP
+);
+
+DROP TABLE IF EXISTS user_roles;
+CREATE TABLE user_roles (
+    userId bigint NOT NULL,
+    roleId integer NOT NULL,
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP,
+    PRIMARY KEY (userId,roleId),
+    FOREIGN KEY (roleId) REFERENCES roles(id),
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
